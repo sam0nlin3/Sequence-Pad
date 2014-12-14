d2518133d66aa64f6e6aed27abc620209d4d06ef
RossWittenberg <ross.wittenberg@gmail.com>
2014-12-13 21:58:53 -0500

    fixed merged conflict on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
 
   def index
   	render layout: 'application', text: ''
   end
 
-||||||| merged common ancestors
-=======
-
-  # This makes the method available in any file in our app
   helper_method :current_user
 
-  # If the current user is not logged in, go to the login page
   def authenticate
     redirect_to root_path unless current_user
   end
 
-  # Returns the user currently logged in
   def current_user
   	User.find(session[:current_user_id]) if session[:current_user_id]
   end
-
->>>>>>> a411bba2200fcf5a3bb573fd5e58c7c862a09b88
 end
