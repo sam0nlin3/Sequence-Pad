19f18bc5d0ce66e4abd7d0f81fd5bd5bd8a3c3ea
SamMatthews9 <SamMatthews9@gmail.com>
2014-12-14 17:45:05 -0500

    commented out the seed file, because it's not functioning properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Song.destroy_all
+User.destroy_all
+Column.destroy_all
+Cell.destroy_all
+
+# # sam = User.create(
+# # 	username: "Sam",
+# # 	password: "password1",
+# # 	password_confirmation: "password1"
+# # 	)
+
+# # ross = User.create(
+# # 	username: "Ross",
+# # 	password: "password1",
+# # 	password_confirmation: "password1"
+# # 	)
+
+# # song1 = Song.new(
+# # 	name: "Test 1",
+# # 	user_id: 1
+# # 	)
+
+# # song1 = Song.new(
+# # 	name: "Test 2",
+# # 	user_id: 2
+# # 	)
+
+# column1 = Column.new
+
+# cell1 = Cell.new
+# cell1.purple_note = 'https://s3.amazonaws.com/spctrl/Boy+Scouts+sleeping+in+a+tent+1.mp3'
+# cell1.purple_active = false
+# cell1.blue_note = 'https://s3.amazonaws.com/spctrl/Boy+Scouts+sleeping+in+a+tent+1.mp3'
+# cell1.blue_active = false
+# cell1.yellow_note = 'https://s3.amazonaws.com/spctrl/Boy+Scouts+sleeping+in+a+tent+1.mp3'
+# cell1.yellow_active = false
+# cell1.green_note = 'https://s3.amazonaws.com/spctrl/Boy+Scouts+sleeping+in+a+tent+1.mp3'
+# cell1.green_active = false
+# # cell1.row = 1
+# # cell1.column = 1
+# cell1.save
+
