da28281b469aa8bd9831b4a83ac57be76259e3db
RossWittenberg <ross.wittenberg@gmail.com>
2014-12-13 22:05:00 -0500

    h1 test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,15 @@ class ApplicationController < ActionController::Base
   	render layout: 'application', text: ''
   end
 
+  # This makes the method available in any file in our app
   helper_method :current_user
 
+  # If the current user is not logged in, go to the login page
   def authenticate
     redirect_to root_path unless current_user
   end
 
+  # Returns the user currently logged in
   def current_user
   	User.find(session[:current_user_id]) if session[:current_user_id]
   end
