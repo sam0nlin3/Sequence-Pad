dc5f4e288626f6dd022e897c040e286a596ead3f
SamMatthews9 <SamMatthews9@gmail.com>
2014-12-16 16:10:37 -0500

    removed some extra lines and commented out code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -141,7 +141,7 @@ function columnLoop() {
 	columnsArray.push( column1, column2, column3, column4, column5, column6,
 	  								 column7, column8, column9, column10, column11, column12,
 	  								 column13, column14, column15, column16 )
-
+	
 	var interval = 0;
 
 	setTimeout(function(){	
