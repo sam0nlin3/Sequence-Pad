7f519d4e5165bb9e02629e447ace484e75c7a1d8
SamMatthews9 <SamMatthews9@gmail.com>
2014-12-17 16:19:19 -0500

    Fixed the login function so it takes a user to the main page and displays their name instead of Log In or Sign Up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:current_user_id] = user.id
+    if @user
+      session[:current_user] = @user.id
       redirect_to root_path
     else
       redirect_to root_path
@@ -20,8 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    binding.pry
-    session[:current_user_id] = nil
+    session[:current_user] = nil
     current_user = nil
     redirect_to root_path
   end
